dc8229e9d995b364c3776da5fcdba7803af36d9d
Honey-Rae Swan <honeyraeswan@gmail.com>
2021-02-02 20:25:28 -0600

    plugged in js data structure

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
+const studentArray = [
+  {
+    studentName: 'Ben',
+    house: 'Ravenclaw',
+  },
+  {
+    studentName: 'Nathan',
+    house: 'Gryffindor',
+  },
+  {
+    studentName: 'Honey-Rae',
+    house: 'Slytherin',
+  },
+];
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId)
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const createStudentCards = () => {
+  let domString = '';
+  studentArray.forEach((item, i) => {
+    domString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">${item.studentName}</h5>
+    <p class="card-text">${item.house}</p>
+    <a href="#" class="btn btn-primary">Expel!</a>
+  </div>
+</div>`;
+  })
+
+  printToDom('#studentcard', domString);
+}
+
+const handleSortEvent = (e) => {
+  e.preventDefault();
+  const studentName = document.querySelector('#studentName').value;
+  console.log(studentName);
+
+  const nameIntoObj = {
+    studentName,
+  }
+  studentArray.push(nameIntoObj);
+  createStudentCards();
+}
+
+
+const sortEvent = (e) => {
+  document.querySelector('#sortButton').addEventListener('click', handleSortEvent);
+}
+
+const init = () => {
+  createStudentCards();
+  sortEvent();
+  
+}
+
+init();
